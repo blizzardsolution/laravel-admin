b6ec98ad41650862952f3ad2b3962854c5384e97
Donatas Tumanas <donatastum@gmail.com>
2017-11-25 21:23:37 +0200

    add migrations +Xethron/MigrationsGenerator

---
+++ database/migrations/2017_11_25_192021_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCompaniesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('companies', function(Blueprint $table)
+		{
+			$table->integer('count', true);
+			$table->string('id', 36)->unique('id_UNIQUE');
+			$table->timestamps();
+			$table->softDeletes();
+			$table->string('name');
+			$table->string('address');
+			$table->string('logo');
+			$table->string('path')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('companies');
+	}
+
+}
