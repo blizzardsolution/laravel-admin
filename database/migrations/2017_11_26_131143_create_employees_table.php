288bed58e3ca8afbde24a58bc652956949f09bfc
Donatas Tumanas <donatastum@gmail.com>
2017-11-26 15:18:56 +0200

    Fix migrations tables rows

---
+++ database/migrations/2017_11_26_131143_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->softDeletes();
+            $table->string('name');
+            $table->string('email');
+            $table->string('company_id', 36)->nullable()->index('fk_employees_companies_idx');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
